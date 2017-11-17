07ba1ce0af3ccbd38e158f45b7886bd747a1478e
baniquedg <baniquedg@104-05-2017.bi.local>
2017-11-17 14:58:02 -0500

    Solid

---
+++ Browser/src/BrowserModel.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class BrowserModel extends Stack<Integer>{
+
+	BrowserModel(BrowserView view){
+		this.view = view;
+	}
+
+	public void back() {
+
+	}
+
+	public void forward() {
+
+	}
+
+	public void home() {
+
+	}
+
+	public void followLink() {
+
+	}
+
+}
