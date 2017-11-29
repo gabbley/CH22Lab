a56c198e84bad6fff34b7aa1c625bd69bc2c99d0
baniquedg <baniquedg@104-05-2017.bi.local>
2017-11-29 12:21:48 -0500

    sad

--- Browser/src/BrowserModel.java
+++ Browser/src/BrowserModel.java
@@ -1,25 +1,47 @@
 import java.util.Stack;
 
-public class BrowserModel extends Stack<Integer>{
+public class BrowserModel {
 
-	BrowserModel(BrowserView view){
+	private BrowserView view;
+	private Stack<Integer> backStack, fwdStack;
+	private Integer home;
+	private Integer lineNum;
+
+	BrowserModel(BrowserView view) {
 		this.view = view;
+		home();
 	}
 
 	public void back() {
-
+		if (hasBack()) {
+			backStack.push(lineNum);
+			lineNum = backStack.pop();
+			view.update(lineNum);
+		}
 	}
 
 	public void forward() {
-
+		
 	}
 
 	public void home() {
-
+		followLink(0);
+		backStack.clear();
 	}
 
-	public void followLink() {
+	public void followLink(int n) {
+		fwdStack.clear();
+		backStack.push(lineNum);
+		lineNum = n;
+		view.update(n);
+	}
 
+	public boolean hasBack() {
+		return !backStack.isEmpty();
+	}
+
+	public boolean hasForward() {
+		return !fwdStack.isEmpty();
 	}
 
 }
